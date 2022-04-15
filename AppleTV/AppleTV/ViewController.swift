494cf13f24f92e41cb2629678e38811b0498813f
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2022-04-15 18:59:08 +0900

    Initial Commit for AppleTV

---
+++ AppleTV/AppleTV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleTV
+//
+//  Created by YOONJONG on 2022/04/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
